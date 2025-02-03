a358538443213ee7cdf206613c16eb5d4f6374d5
Dr Alban THIKA <alban_thika@msn.com>
2025-02-03 17:09:07 +0100

    Update NOVARIC.sol
    Signed-off-by: Dr Alban THIKA <alban_thika@msn.com>

--- contracts/artifacts/NOVARIC.sol/NOVARIC.sol
+++ contracts/artifacts/NOVARIC.sol/NOVARIC.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.20;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20FlashMint} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
-import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
-import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/releases/download/v5.0.1/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/releases/download/v5.0.1/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/releases/download/v5.0.1/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/releases/download/v5.0.1/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/releases/download/v5.0.1/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/releases/download/v5.0.1/contracts/access/Ownable.sol";
 
 /// @custom:security-contact security@novaric.co
 contract NOVARIC is ERC20, ERC20Pausable, Ownable, ERC20Permit, ERC20Votes, ERC20FlashMint {
@@ -41,12 +40,7 @@ contract NOVARIC is ERC20, ERC20Pausable, Ownable, ERC20Permit, ERC20Votes, ERC2
         super._update(from, to, value);
     }
 
-    function nonces(address owner)
-        public
-        view
-        override(ERC20Permit, Nonces)
-        returns (uint256)
-    {
-        return super.nonces(owner);
+    function _getVotingUnits(address account) internal view override returns (uint256) {
+        return balanceOf(account);
     }
 }
